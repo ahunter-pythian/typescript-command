d8d6a73c80f815de6f6328b0ded2c5cdf08ab908
Anthony Hunter <ahunterhunter@gmail.com>
2019-01-09 00:08:23 -0500

    Added CommandLoader.ts

---
+++ src/commands/CommandLoader.ts
@@ -0,0 +1,19 @@
+/*************************************************************************************************************************
+ * Copyright (c) 2018, Anthony Hunter.
+ * This unpublished material is proprietary to Anthony Hunter.
+ * All rights reserved. The methods and techniques described herein are considered trade secrets and/or confidential.
+ * Reproduction or distribution, in whole or in part, is forbidden except by express written permission of Anthony
+ * Hunter.
+ *************************************************************************************************************************/
+import {CommanderStatic} from "commander";
+import {HelloAction} from "../actions/HelloAction";
+import {QuestionAction} from "../actions/QuestionAction";
+import {HelloCommand} from "./HelloCommand";
+import {QuestionCommand} from "./QuestionCommand";
+
+export class CommandLoader {
+    public static load(program: CommanderStatic): void {
+        new HelloCommand(new HelloAction()).load(program);
+        new QuestionCommand(new QuestionAction()).load(program);
+    }
+}
