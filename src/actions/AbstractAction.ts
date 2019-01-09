74b1d709046c0bdac8370d43c132e7e2c27b35d2
Anthony Hunter <ahunterhunter@gmail.com>
2019-01-09 00:08:18 -0500

    Added AbstractAction.ts

---
+++ src/actions/AbstractAction.ts
@@ -0,0 +1,11 @@
+/*************************************************************************************************************************
+ * Copyright (c) 2019, Anthony Hunter.
+ * This unpublished material is proprietary to Anthony Hunter.
+ * All rights reserved. The methods and techniques described herein are considered trade secrets and/or confidential.
+ * Reproduction or distribution, in whole or in part, is forbidden except by express written permission of Anthony
+ * Hunter.
+ *************************************************************************************************************************/
+
+export abstract class AbstractAction {
+    public abstract async handle(...args): Promise<void>;
+}
