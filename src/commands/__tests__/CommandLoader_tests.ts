eec03bbc2c7533a7d80d219cf9bdb3704a80bec6
Anthony Hunter <ahunterhunter@gmail.com>
2019-12-28 21:39:47 -0500

    Updated CommandLoader_tests to add correct file header

--- src/commands/__tests__/CommandLoader_tests.ts
+++ src/commands/__tests__/CommandLoader_tests.ts
@@ -1,9 +1,10 @@
-/*******************************************************************************
- * Copyright 2019 The Pythian Group Inc.
- * All Rights Reserved.
- *******************************************************************************/
-//
-// Author: Anthony Hunter
+/*************************************************************************************************************************
+ * Copyright (c) 2019, Anthony Hunter.
+ * This unpublished material is proprietary to Anthony Hunter.
+ * All rights reserved. The methods and techniques described herein are considered trade secrets and/or confidential.
+ * Reproduction or distribution, in whole or in part, is forbidden except by express written permission of Anthony
+ * Hunter.
+ *************************************************************************************************************************/
 import * as commander from "commander";
 import {CommanderStatic} from "commander";
 import {CommandLoader} from "../CommandLoader";
